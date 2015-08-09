f65128b08d98ac3704433765f75a801237aa44e6
Rodrigo Troncoso <rod.tronco@gmail.com>
2015-08-09 01:57:16 -0300

    Better use of async with named tasks

--- common/models/ml-payment.js
+++ common/models/ml-payment.js
@@ -6,36 +6,39 @@ module.exports = function(Payment) {
   Payment.observe('before save', function(ctx, next) {
     var model = ctx.instance;
 
-    async.parallel([
-      function updateModel(callback) {
-        model.mlId = model.id;
-        model.unsetAttribute('id');
-
-        callback();
-      },
-      function processPayment(callback) {
-
-        var mp = Payment.app.mp;
-
-        mp.getPayment(model.mlId, function(err, data) {
-          handlePaymentInfo(err, data, callback, model);
-          callback();
-        });
-      }
-    ]);
+    async.parallel({
+      update: updateModel.bind(null, model),
+      process: processPayment.bind(null, model)
+    });
 
     next();
   });
 
-  function handlePaymentInfo(err, data, callback, model) {
+  var updateModel = function(model, cb) {
+    model.mlId = model.id;
+    model.unsetAttribute('id');
+
+    cb();
+  }
+
+  var processPayment = function(model, cb) {
+    var mp = Payment.app.mp;
+
+    mp.getPayment(model.mlId, function (err, data) {
+      handlePaymentInfo(err, data, cb, model);
+      cb();
+    });
+  }
+
+  function handlePaymentInfo(err, data, cb, model) {
     if(err) {
       throw new err;
     }
 
-    console.log(data);
-    model.updateAll({ id: model.id }, data, function(err, results) {
+    console.log(data.response.collection);
+    model.updateAll({ id: model.id }, data.response.collection, function(err, results) {
       if(err) {
-        callback(err);
+        cb(err);
       }
 
       console.log(results);
