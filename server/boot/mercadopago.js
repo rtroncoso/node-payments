6a5a2cb92b9c2614fd9df1e79657fa4c1191a094
Rodrigo Troncoso <rod.tronco@gmail.com>
2015-08-09 01:17:35 -0300

    Add sandbox route for payment testing

--- server/boot/mercadopago.js
+++ server/boot/mercadopago.js
@@ -15,4 +15,26 @@ module.exports = function(app) {
     }, function(err) {
       console.log('Unhandled MP error: ' + err.message);
     });
+
+  app.use('/sandbox', function(req, res) {
+    var preference = {
+      "items": [
+        {
+          "title": "Test",
+          "quantity": 1,
+          "currency_id": "USD",
+          "unit_price": 10.5
+        }
+      ]
+    };
+
+    // Sandbox preference point
+    app.mp.createPreference(preference).then(function(data) {
+      if(data.status === 201) {
+        res.redirect(data.response.sandbox_init_point);
+      }
+    }, function(err) {
+      throw new err;
+    });
+  });
 };
